71d8d20d45c37a8332459888773a6d4393e9d193
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-11-11 12:18:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+// add more points in JSON format
+var points = [
+  { x: 400, y: 200 },
+
+];
+
+function preload(){
+  bgImg = loadImage("assets/background.png")
+}
+function setup() {
+  createCanvas(850, 400);
+
+  
+
+ 
+}
+
+function draw() {
+  // set background image
+  background("black");
+
+  // write a for loop here
+  drawPoints();
+
+
+
+
+
+}
+
+
+
+function drawPoints() {
+  push();
+  stroke("white")
+  strokeWeight(3);
+  ellipseMode(RADIUS);
+  fill("blue");
+  ellipse(200,200, 10, 10);
+  pop();
+}
+
